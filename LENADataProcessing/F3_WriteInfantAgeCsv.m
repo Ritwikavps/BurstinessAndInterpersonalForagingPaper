73e76fb002d14dacaeb277564c7796d2aa391394
Ritwikavps <38600536+Ritwikavps@users.noreply.github.com>
2022-01-26 14:31:14 -0600

    Add files via upload

---
+++ LENADataProcessing/F3_WriteInfantAgeCsv.m
@@ -0,0 +1,50 @@
+clear all
+clc
+
+%Ritwika VPS, (UCLA, UC Merced)
+%Script to get infant age from .csv files with DOB and time of recording
+%and write the info into a single .csv file
+
+%go to folder ith .csv files
+cd '/Volumes/GoogleDrive/My Drive/research/vocalisation/Pre_registration_followu/Data/LENAData/ItsFiles'
+
+aa = dir('*InfantAge.csv'); %get all relevant .csv files
+
+if isempty(aa) %chekc if there are .csv files
+    error('There are no .csv files ending with InfantAge.csv')
+end
+
+%read csv file with its file details to match age and file name to infant ID
+%This will be usefiul in writing metadata
+%The options are being specified because we want the infant id to be read
+%in as string so that infant ID 009 is read in as 009 and not 9 (for
+%example)
+%Alao because there are IDs 384 A and B
+opts = detectImportOptions('ItsFileDetails.csv');
+opts = setvartype(opts, 'InfantCode', 'string');
+ItsFileTab = readtable('ItsFileDetails.csv',opts);
+
+for i = 1:numel(aa) %go through .csv files
+    AgeTab = readtable(aa(i).name); 
+    
+    %get recording date; note that it is in year-month-day format
+    %strrep replaces substring in first '' by substring in the second ''
+    %datenum converts the date to a number, to do arithmetic operations on
+    RecDate = datenum(strrep(AgeTab{1,1},'Recording date is',''),'yyyy-mm-dd');
+    DOBdate = datenum(strrep(AgeTab.Properties.VariableNames{1},'DOBIs',''),'yyyy_mm_dd'); %get DOB (variable name)
+    
+    InfantAge(i,1) = abs(RecDate-DOBdate);
+    FileNameRoot{i,1} = strrep(aa(i).name,'InfantAge.csv',''); %get file name root
+    
+    for j = 1:size(ItsFileTab,1) %loop through the rows to match file names
+        if contains(ItsFileTab{j,1},FileNameRoot{i,1}) %if fileroot matches
+            InfantID(i,1) = ItsFileTab{j,2};
+        end
+    end
+    
+end
+
+T = table(FileNameRoot,InfantID,InfantAge);
+writetable(T,'/Volumes/GoogleDrive/My Drive/research/vocalisation/Pre_registration_followu/Data/MetadataInfAgeAndID.csv')
+
+delete *InfantAge.csv
