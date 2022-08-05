ba48809b1f514b5778fb71b29a2c09c9e89f29af
Ritwikavps <38600536+Ritwikavps@users.noreply.github.com>
2022-08-05 02:47:50 -0500

    Add files via upload

--- A2_HUMLabelDataCleanUp/GetAnnotInOrthoButNotInUttDir.m
+++ A2_HUMLabelDataCleanUp/GetAnnotInOrthoButNotInUttDir.m
@@ -39,63 +39,71 @@ UniqCheck = [abs(numel(T_AdultUttDirAnnot.StartTimeVal)-numel(unique(T_AdultUttD
 if sum(UniqCheck) > 0
     fprintf('There are duplicate time values in %s \n',EafFilename) %verified that there are no duplicate time entries
 end
+ 
+if ~isempty(T_AdultUttDirAnnot) %only proceed if utt dir table not empty
 
-%now go through the orthographic table start and end times and compare to adult utterance direction start and end time
-for j = 1:numel(T_AdultOrthoTranscrip.StartTimeRef)
-    if (~ismember(T_AdultOrthoTranscrip.StartTimeVal(j),T_AdultUttDirAnnot.StartTimeVal)) ...
-            || (~ismember(T_AdultOrthoTranscrip.EndTimeVal(j),T_AdultUttDirAnnot.EndTimeVal)) %if either start or end time of any orthographic annotation
-        %is missing in the utterance direction tier, add to the table
-        %keeping track of this
-        T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_AdultOrthoTranscrip(j,:)];
-    end 
+    if ~isempty(T_AdultOrthoTranscrip)
+        %now go through the orthographic table start and end times and compare to adult utterance direction start and end time
+        for j = 1:numel(T_AdultOrthoTranscrip.StartTimeRef)
+            if (~ismember(T_AdultOrthoTranscrip.StartTimeVal(j),T_AdultUttDirAnnot.StartTimeVal)) ...
+                    || (~ismember(T_AdultOrthoTranscrip.EndTimeVal(j),T_AdultUttDirAnnot.EndTimeVal)) %if either start or end time of any orthographic annotation
+                %is missing in the utterance direction tier, add to the table
+                %keeping track of this
+                T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_AdultOrthoTranscrip(j,:)];
+            end 
+        end
+    end
+    
+    %------------------------------------------------------------------------------------------------------------------------------------------------------------
+    if ~isempty(T_Music)
+        %go through music tier and check if there are music tier annotationns that
+        %are not present in the adult utterance direction tier
+        for j = 1:numel(T_Music.StartTimeRef)
+            if (~ismember(T_Music.StartTimeVal(j),T_AdultUttDirAnnot.StartTimeVal)) ...
+                    || (~ismember(T_Music.EndTimeVal(j),T_AdultUttDirAnnot.EndTimeVal)) %if either start or end time of any music annotation
+                %is missing in the utterance direction tier, add to the table
+                %keeping track of this
+                T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_Music(j,:)];
+            end 
+        end
+        
+        %go through utterance direction tier and check if there are utterance direction tier annotationns that
+        %are not present in the music tier
+        for j = 1:numel(T_AdultUttDirAnnot.StartTimeRef)
+            if (~ismember(T_AdultUttDirAnnot.StartTimeVal(j),T_Music.StartTimeVal)) ...
+                    || (~ismember(T_AdultUttDirAnnot.EndTimeVal(j),T_Music.EndTimeVal)) %if either start or end time of any utterance dir annotation
+                %is missing in the music tier, add to the table
+                %keeping track of this
+                T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_AdultUttDirAnnot(j,:)];
+            end 
+        end
+    end
+    
+    %------------------------------------------------------------------------------------------------------------------------------------------------------------
+    if ~isempty(T_BgOlp)
+        %Do simiarlly for background overlap tier and utterance direction tier
+        %go through background olp tier and check if there are background olp tier annotationns that
+        %are not present in the adult utterance direction tier
+        for j = 1:numel(T_BgOlp.StartTimeRef)
+            if (~ismember(T_BgOlp.StartTimeVal(j),T_AdultUttDirAnnot.StartTimeVal)) ...
+                    || (~ismember(T_BgOlp.EndTimeVal(j),T_AdultUttDirAnnot.EndTimeVal)) %if either start or end time of any background olp annotation
+                %is missing in the utterance direction tier, add to the table
+                %keeping track of this
+                T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_BgOlp(j,:)];
+            end 
+        end
+    
+        %go through utterance direction tier and check if there are utterance direction tier annotationns that
+        %are not present in the backgroiund olp tier
+        for j = 1:numel(T_AdultUttDirAnnot.StartTimeRef)
+            if (~ismember(T_AdultUttDirAnnot.StartTimeVal(j),T_BgOlp.StartTimeVal)) ...
+                    || (~ismember(T_AdultUttDirAnnot.EndTimeVal(j),T_BgOlp.EndTimeVal)) %if either start or end time of any utterance dir annotation
+                %is missing in the background olp tier, add to the table
+                %keeping track of this
+                T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_AdultUttDirAnnot(j,:)];
+            end 
+        end
+    end
 end
-
-%------------------------------------------------------------------------------------------------------------------------------------------------------------
-%go through music tier and check if there are music tier annotationns that
-%are not present in the adult utterance direction tier
-for j = 1:numel(T_Music.StartTimeRef)
-    if (~ismember(T_Music.StartTimeVal(j),T_AdultUttDirAnnot.StartTimeVal)) ...
-            || (~ismember(T_Music.EndTimeVal(j),T_AdultUttDirAnnot.EndTimeVal)) %if either start or end time of any music annotation
-        %is missing in the utterance direction tier, add to the table
-        %keeping track of this
-        T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_Music(j,:)];
-    end 
-end
-
-%go through utterance direction tier and check if there are utterance direction tier annotationns that
-%are not present in the music tier
-for j = 1:numel(T_AdultUttDirAnnot.StartTimeRef)
-    if (~ismember(T_AdultUttDirAnnot.StartTimeVal(j),T_Music.StartTimeVal)) ...
-            || (~ismember(T_AdultUttDirAnnot.EndTimeVal(j),T_Music.EndTimeVal)) %if either start or end time of any utterance dir annotation
-        %is missing in the music tier, add to the table
-        %keeping track of this
-        T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_AdultUttDirAnnot(j,:)];
-    end 
-end
-
-%------------------------------------------------------------------------------------------------------------------------------------------------------------
-%Do simiarlly for background overlap tier and utterance direction tier
-%go through background olp tier and check if there are background olp tier annotationns that
-%are not present in the adult utterance direction tier
-for j = 1:numel(T_BgOlp.StartTimeRef)
-    if (~ismember(T_BgOlp.StartTimeVal(j),T_AdultUttDirAnnot.StartTimeVal)) ...
-            || (~ismember(T_BgOlp.EndTimeVal(j),T_AdultUttDirAnnot.EndTimeVal)) %if either start or end time of any background olp annotation
-        %is missing in the utterance direction tier, add to the table
-        %keeping track of this
-        T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_BgOlp(j,:)];
-    end 
-end
-
-%go through utterance direction tier and check if there are utterance direction tier annotationns that
-%are not present in the backgroiund olp tier
-for j = 1:numel(T_AdultUttDirAnnot.StartTimeRef)
-    if (~ismember(T_AdultUttDirAnnot.StartTimeVal(j),T_BgOlp.StartTimeVal)) ...
-            || (~ismember(T_AdultUttDirAnnot.EndTimeVal(j),T_BgOlp.EndTimeVal)) %if either start or end time of any utterance dir annotation
-        %is missing in the background olp tier, add to the table
-        %keeping track of this
-        T_AnnotInOrthoTierButNotAdultUttDirTier = [T_AnnotInOrthoTierButNotAdultUttDirTier; T_AdultUttDirAnnot(j,:)];
-    end 
-end
-
 
 T_AnnotInOrthoTierButNotAdultUttDirTier_out = T_AnnotInOrthoTierButNotAdultUttDirTier;
